36e17fe07989d2020891b2b86ed6a2b581f57b64
BatoolBtoush <batoolbtoush98@gmail.com>
2022-05-20 22:04:01 +0300

    completed code

--- hashtable/tests/test_hashtable.py
+++ hashtable/tests/test_hashtable.py
@@ -1,6 +1,14 @@
 from hashtable.hashtable import HashTable
 import pytest
 
+# All tests are satisfied
+# 1. Setting a key/value to your hashtable results in the value being in the data structure
+# 2. Retrieving based on a key returns the value stored
+# 3. Successfully returns null for a key that does not exist in the hashtable
+# 4. Successfully returns a list of all unique keys that exist in the hashtable
+# 5. Successfully handle a collision within the hashtable
+# 6. Successfully retrieve a value from a bucket within the hashtable that has a collision
+# 7. Successfully hash a key to an in-range value
 
 
 # test hash method
@@ -10,7 +18,8 @@ def test_hash():
     expected = 755
     assert actual == expected
 
-#test set and get methods
+
+# test set and get methods
 def test_set_and_get(hashtable):
     assert hashtable.get("name") == "Batool Ragayah"
     assert hashtable.get("batool") == "ragayah"
@@ -19,19 +28,27 @@ def test_set_and_get(hashtable):
     assert hashtable.get("bcdefa") == "2"
     assert hashtable.get("cdefab") == "3"
     assert hashtable.get("defabc") == "4"
+    assert hashtable.get("doesnt exist") == None
 
-#test contains method
+
+# test contains method
 def test_contains(hashtable):
     assert hashtable.contains("name") == True
     assert hashtable.contains("batool") == True
     assert hashtable.contains("Python") == False
 
 
-#test keys method
+# test keys method
 def test_keys(hashtable):
-    assert hashtable.keys() == ['abcdef', 'bcdefa', 'cdefab', 'defabc', 'course', 'name', 'batool']
-
-
+    assert hashtable.keys() == [
+        "abcdef",
+        "bcdefa",
+        "cdefab",
+        "defabc",
+        "course",
+        "name",
+        "batool",
+    ]
 
 
 @pytest.fixture
